3fbb8b9a5ad0e1c539d76e4e39e86f563ea8490b
Gurpreet Parmar <parmarg@gmail.com>
2013-01-13 12:28:12 -0800

    setup crowd_config

--- lib/devise_crowd_authenticatable.rb
+++ lib/devise_crowd_authenticatable.rb
@@ -10,30 +10,16 @@ require 'devise_crowd_authenticatable/routes'
 # Get ldap information from config/ldap.yml now
 module Devise
   # Allow logging
-  mattr_accessor :ldap_logger
-  @@ldap_logger = true
+  mattr_accessor :crowd_logger
+  @@cro_logger = true
   
   # Add valid users to database
-  mattr_accessor :ldap_create_user
-  @@ldap_create_user = false
+  mattr_accessor :d_create_user
+  @@crowd_create_user = false
   
-  mattr_accessor :ldap_config
+  mattr_accessor :crowd_config
   # @@ldap_config = "#{Rails.root}/config/ldap.yml"
-  
-  mattr_accessor :ldap_update_password
-  @@ldap_update_password = true
-  
-  mattr_accessor :ldap_check_group_membership
-  @@ldap_check_group_membership = false
-  
-  mattr_accessor :ldap_check_attributes
-  @@ldap_check_role_attribute = false
-  
-  mattr_accessor :ldap_use_admin_to_bind
-  @@ldap_use_admin_to_bind = false
-  
-  mattr_accessor :ldap_auth_username_builder
-  @@ldap_auth_username_builder = Proc.new() {|attribute, login, ldap| "#{attribute}=#{login},#{ldap.base}" }
+
 end
 
 # Add ldap_authenticatable strategy to defaults.
