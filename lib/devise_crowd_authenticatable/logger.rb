bec4247cbcfc4fa5c079cd6c04db63a056d7e517
Gurpreet Parmar <parmarg@gmail.com>
2013-01-13 12:33:32 -0800

    futher fixes

--- lib/devise_crowd_authenticatable/logger.rb
+++ lib/devise_crowd_authenticatable/logger.rb
@@ -1,8 +1,8 @@
-module DeviseLdapAuthenticatable
+module DeviseCrowdAuthenticatable
 
   class Logger    
     def self.send(message, logger = Rails.logger)
-      if ::Devise.ldap_logger
+      if ::Devise.crowd_logger
         logger.add 0, "  \e[36mLDAP:\e[0m #{message}"
       end
     end
